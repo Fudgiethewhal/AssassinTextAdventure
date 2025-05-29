5e8c7633b5b8725eef19dc3bd02f85cc0a5898c9
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-05-28 23:23:55 -0700

    completed combat class

--- AssassinTextAdventureUI/GameFunctions/Combat.cs
+++ AssassinTextAdventureUI/GameFunctions/Combat.cs
@@ -1,6 +1,37 @@
-﻿namespace AssassinTextAdventureUI.GameFunctions;
+﻿using AssassinTextAdventureUI.Character;
 
-public class Combat
+namespace AssassinTextAdventureUI.GameFunctions;
+
+public static class Combat
 {
-    
+    public static void KnightVsOneEnemy(Assassin assassin, Enemy enemy)
+    {
+        while (assassin.Health > 0 && enemy.Health > 0)
+        {
+            UserInteraction.GameDialogue($"\n{assassin.Name} (Health: {assassin.Health} Attack: {assassin.AttackDamage} vs {enemy.Name}) Health: {enemy.Health} Attack: {enemy.AttackDamage})");
+            Thread.Sleep(5000);
+            
+            UserInteraction.GameDialogue($"{assassin.Name} attacks {enemy.Name}!");
+            enemy.TakeDamage(assassin.Attack());
+            Thread.Sleep(5000);
+
+            if (enemy.Health <= 0)
+            {
+                UserInteraction.GameDialogue($"{enemy.Name} has been defeated");
+                    Thread.Sleep(5000);
+                    break; 
+            }
+            
+            UserInteraction.GameDialogue($"{enemy.Name} attacks {assassin.Name}!");
+            assassin.TakeDamage(enemy.Attack());
+            Thread.Sleep(5000);
+            
+            if (assassin.Health <= 0)
+            {
+                UserInteraction.GameDialogue($"{assassin.Name} has been defeated!");
+                Thread.Sleep(5000);
+                break; 
+            }
+        }
+    }
 }
