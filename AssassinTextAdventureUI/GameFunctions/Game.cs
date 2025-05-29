7ab7f4df7053244248149b2026b441b398947d96
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-05-28 23:53:47 -0700

    calling the method in program

--- AssassinTextAdventureUI/GameFunctions/Game.cs
+++ AssassinTextAdventureUI/GameFunctions/Game.cs
@@ -15,7 +15,20 @@ public static class Game
 
         UserInteraction.GameDialogue($"Hello and welcome to the Creed{assassin.Name}, I see you are the new recruit.\nWould you like to ask me something?"); 
         UserInteraction.PlayerInquiry(new string[] {"Who are you?", "What is this place?", "Goodbye"}, new string[] {"I am the commander of this lair. My name isn't relevant.", "This is the great city of Cairo, but danger lurks at every corner..."});
-     }
+        Thread.Sleep(2000);
+        
+        UserInteraction.GameDialogue("You hear footsteps approaching..");
+        Thread.Sleep(2000);
+
+        var templar01 = new Enemy();
+        templar01.Name = "Queso";
+        templar01.Health = StatGeneration.GenerateRandomHealth();
+        templar01.AttackDamage = StatGeneration.GenerateRandomEnemyAttack();
+        
+        Combat.KnightVsOneEnemy(assassin, templar01);
+        
+        UserInteraction.GameDialogue("You have survived, brave assassin!");
+    }
     
 }
 
